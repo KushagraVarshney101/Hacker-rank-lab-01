120528dc692123328c3fa502102ff66b48efe6c6
KushagraVarshney101 <114856143+KushagraVarshney101@users.noreply.github.com>
2023-02-27 22:30:01 +0530

    Add files via upload

---
+++ floor an ceil .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    float a;
+    scanf("%f",&a);
+    printf("%f\n",floor(a));
+    printf("%f\n",ceil(a));
+    return 0;
+}
---
+++ math function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double a,b;
+    double c;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    scanf("%lf",&c);
+    printf("%lf\n",pow(a,b));
+    printf("%lf",sqrt(c));
+    return 0;
+}
